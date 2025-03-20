ed77d509a427661d757cf2e8cc55fab6524344d7
asqel <axel.lleres@gmail.com>
2025-03-20 15:38:28 +0100

    update

--- src/textures.c
+++ src/textures.c
@@ -35,7 +35,6 @@ void	init_textures(t_context *ctx)
 	try_load_texture(ctx, &(ctx->floor_sprite), "assets/ground.xpm");
 	try_load_texture(ctx, &(ctx->wall_sprite), "assets/wall.xpm");
 	try_load_texture(ctx, &(ctx->exit_sprite), "assets/exit.xpm");
-	try_load_texture(ctx, &(ctx->coin_sprite), "assets/coin.xpm");
 	try_load_texture(ctx, &(ctx->player_sprite[0][0]), "assets/player_u.xpm");
 	try_load_texture(ctx, &(ctx->player_sprite[1][0]), "assets/player_r.xpm");
 	try_load_texture(ctx, &(ctx->player_sprite[2][0]), "assets/player_d.xpm");
